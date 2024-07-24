278db3c7b73423fb50e224a0ab904b91f6d6ea2d
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-07-24 16:05:41 +0200

    project re-do initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import colors from 'colors';
+import * as dotenv from 'dotenv';
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const Port = process.env.PORT || 5022;
+
+app.listen(() => {
+  console.log(`server running on port: ${Port}`.bgCyan);
+});

ff9649386cc730e167be8606b632fe5c08f1c6e0
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-07-25 12:02:28 +0200

    update of index.js and Readme

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import colors, { black } from 'colors';
+import colors from 'colors';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config();
+
+const { black } = colors;
 const app = express();
 
 app.use(cors());
@@ -15,4 +19,26 @@ app.listen(() => {
   console.log(`server running on port: ${Port}`.bgCyan);
 });
 
-black;
+const DBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.Mongo_DB);
+    console.log('connection with MongoDB established'.bgGreen);
+  } catch (error) {
+    console.log('problem with connecting to MongoDB'.bgRed, error);
+  }
+};
+
+// bellow is a main function that runs the server aka IIFE:
+//* Immediatelly Invoked Function Expression
+
+(async function controller() {
+  const app = express();
+  await DBConnection();
+  addMiddlewares(app);
+  loadRoutes(app);
+  startServer(app);
+})();
+// a self-contained function that initializes an Express application, establishes a DB connection,
+// adds necessary middleware, sets up routes, and starts the server.
+// The use of an IIFE ensures that all these steps are executed immediately
+// when the script is run, and the use of async/await ensures that asynchronous operations are handled correctly.

c8e6851ecfa5583c8178603a9b66400e02cbd18d
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-01 16:10:53 +0200

    updates to App, main, Navbar, Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,29 @@ import Navbar from './components/Navbar';
 import DesktopImage from './assets/images/bobbleheads.png';
 import Footer from './components/Footer';
 import { AuthProvider } from './context/AuthContext';
+import Routes from './routes/Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   const [state, setState] = useState(0);
 
   return (
-    // <Router>
-    <AuthProvider>
-      <div className='flex flex-col min-h-screen'>
-        <Navbar />
-        <div
-          className='sti flex-grow bg-cover bg-center'
-          style={{ backgroundImage: `url(${DesktopImage})` }}
-        >
-          <div className='flex-grow'>{/* <Routes /> */}</div>
+    <Router>
+      <AuthProvider>
+        <div className='flex flex-col min-h-screen'>
+          <Navbar />
+          <div
+            className='sti flex-grow bg-cover bg-center'
+            style={{ backgroundImage: `url(${DesktopImage})` }}
+          >
+            <div className='flex-grow'>
+              <Routes />
+            </div>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </AuthProvider>
-    // </Router>
+      </AuthProvider>
+    </Router>
   );
 }
 

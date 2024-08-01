14f00f41218384c05d8bee20994d50eacbe688c3
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-01 15:24:35 +0200

    +SingleProduct, Routes expand

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -3,18 +3,50 @@ import Home from '../pages/Home';
 import Products from '../pages/Products';
 import AboutUs from '../pages/AboutUs';
 import MyAccount from '../pages/MyAccount';
+import Login from '../pages/Login';
+import Logout from '../pages/Logout';
+import Register from '../pages/Register';
+import SingleProduct from '../pages/SingleProduct';
+import NotFound from '../pages/NotFound';
 
 const Routes = () => {
-    return {
-        <RouterRoutes>
-            <Route path='/' element={<Home />} />
+  return (
+    <RouterRoutes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/logout' element={<Logout />} />
+      <Route path='/register' element={<Register />} />
 
-        </RouterRoutes>
-                
+      <Route
+        path='/products'
+        element={
+          // <ProtectedRoute>
+          <Products />
+          // </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path='/products/:productid'
+        element={
+          //   <ProtectedRoute>
+          <SingleProduct />
+          //   </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path='/myaccount'
+        element={
+          //   <ProtectedRoute>
+          <MyAccount />
+          //   </ProtectedRoute>
+        }
+      />
 
-    };
+      <Route path='*' element={<NotFound />} />
+    </RouterRoutes>
+  );
 };
 
 export default Routes;

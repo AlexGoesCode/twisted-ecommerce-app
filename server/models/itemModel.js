0d8204ddea79aab334b7f3fa74e5e69b7d6898bb
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-03 20:36:04 +0200

    upd: itemModel.js

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -3,27 +3,29 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 // default is false
-const itemSchema = new Schema({
-  name: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
+const itemSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      unique: true,
     },
-  ],
-});
+    body: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const itemModel = mongoose.model('Item', itemSchema);
-
 export default itemModel;

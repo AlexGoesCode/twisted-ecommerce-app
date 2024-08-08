d3c9466ef70bd9bdc299247de04c008168f8a7a0
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-08 11:35:03 +0200

    +Basket.tsx

---
+++ client/src/pages/Basket.tsx
@@ -0,0 +1,11 @@
+import ShoppingCart from '../components/ShoppingCart';
+
+function Basket() {
+  return (
+    <div className='container mx-auto p-4'>
+      <ShoppingCart />
+    </div>
+  );
+}
+
+export default Basket;

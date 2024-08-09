0d860334e1a730fbf9ff4f30202f78385c2e3fde
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-09 18:55:20 +0200

    fixed adding products to cart (counted) in ItemsController

--- server/controller/itemsController.js
+++ server/controller/itemsController.js
@@ -51,29 +51,82 @@ const getItemById = async (req, res) => {
 };
 
 const addProductToCart = async (req, res) => {
+  console.log('add to cart runnning');
   try {
     // Extract user from request
     const user = req.user;
     // Extract product ID from request body
     const { productId } = req.body;
+    console.log('user.shoppingCart :>> ', user.shoppingCart.length);
 
     if (!productId) {
+      console.log('ln61');
       return res.status(400).json({ message: 'Product ID is required' });
     }
-    // Find user in the database
-    const userRecord = await userModel.findByIdAndUpdate(user._id);
 
-    if (!userRecord) {
+    if (!user) {
+      console.log('ln65');
       return res.status(404).json({ message: 'User not found ' });
     }
-    // add product ID to user's shop.cart
-    userRecord.shoppingCart.push(productId);
 
-    // save updated document
-    await userRecord.save();
+    // Check if the product is already in the cart
 
-    // send success response
-    res.status(200).json({ message: 'Product added to cart successfully ' });
+    const existingItem = user.shoppingCart.find((item) => {
+      return item.product.toString() === productId;
+    });
+    console.log('existingItem :>> ', existingItem);
+    if (existingItem) {
+      console.log('existing item ln 86');
+      const newQuantity = existingItem.quantity + 1;
+
+      // If item exists, increase the quantity
+      try {
+        const user2 = await userModel.findOneAndUpdate(
+          { _id: user._id, 'shoppingCart.product': productId },
+
+          {
+            $set: {
+              'shoppingCart.$.product': productId,
+              'shoppingCart.$.quantity': newQuantity,
+            },
+          },
+
+          { new: true }
+        );
+        return res.status(200).json({
+          message: 'Product added to cart successfully, and quantity updated ',
+
+          user2,
+        });
+      } catch (error) {
+        console.log(
+          'error, adding product when product already exist in cart :>> ',
+          error
+        );
+      }
+    }
+
+    if (!existingItem) {
+      // If item does not exist, we update the quantity property in Items collection and push the id of the product to the shoppingCart array
+      try {
+        const user3 = await userModel.updateOne(
+          { _id: user._id },
+          { $addToSet: { shoppingCart: { quantity: 1, product: productId } } },
+          { new: true }
+        );
+        // send success response
+        return res.status(200).json({
+          message: 'Product added to cart successfully, and quantity updated ',
+
+          user3,
+        });
+      } catch (error) {
+        console.log(
+          'error, adding product when product DO NOT exist in cart already :>> ',
+          error
+        );
+      }
+    }
   } catch (error) {
     console.error('Error adding product to cart:', error);
     res.status(500).json({ message: 'Internal server error' });

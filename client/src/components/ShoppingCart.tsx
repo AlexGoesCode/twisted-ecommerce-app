76121db733459f55142433d5b7e811fd424f8b62
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-12 18:12:09 +0200

    + ShoppingCart.tsx page

--- client/src/components/ShoppingCart.tsx
+++ client/src/components/ShoppingCart.tsx
@@ -1,36 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
+import { useShoppingCart } from '../hooks/useShoppingCart';
+import { Item, UserType } from '../types/Types';
+import Products from '../pages/Products';
+import { useNavigate } from 'react-router-dom';
 
 function ShoppingCart() {
   const { user } = useAuth();
+  const token = localStorage.getItem('token') || ''; //* does it make sense?
+  // console.log('token :>> ', token);
+  const {
+    cartItems,
+    fetchCart,
+    addItemsToCart,
+    removeItemsFromCart,
+    deleteItemsFromCart,
+  } = useShoppingCart(token);
+  const navigate = useNavigate();
+
+  console.log('cartItems :>> ', cartItems);
+  useEffect(() => {
+    if (token) {
+      fetchCart();
+    }
+  }, [fetchCart, token]);
+
+  // const handleCheckout = () => {
+  // Implement checkout logic here
+  //   alert('Proceeding to checkout');
+  // };
+
+  // const handleRemoveItem = (id: number) => {
+  //   // setCartItems(cartItems.filter((item) => item.id !== id));
+  // };
+
+  const increaseQuantity = (productId: string) => {
+    addItemsToCart(productId);
+  };
+
+  const decreaseQuantity = (productId: string) => {
+    removeItemsFromCart(productId);
+  };
+
+  const deleteItem = (productId: string) => {
+    deleteItemsFromCart(productId);
+  };
 
   const handleCheckout = () => {
-    // Implement checkout logic here
-    alert('Proceeding to checkout');
+    navigate('/checkout');
   };
-
-  const handleRemoveItem = (id: number) => {
-    // setCartItems(cartItems.filter((item) => item.id !== id));
-  };
-
-  const increaseQuantity = (id: number) => {
-    // setCartItems(
-    //   cartItems.map((item) =>
-    //     item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-    //   )
-    // );
-  };
-
-  const decreaseQuantity = (id: number) => {
-    // setCartItems(
-    //   cartItems.map((item) =>
-    //     item.id === id && item.quantity > 1
-    //       ? { ...item, quantity: item.quantity - 1 }
-    //       : item
-    //   )
-    // );
-  };
-
   // const totalPrice = cartItems.reduce(
   //   (total, item) => total + item.price * item.quantity,
   //   0
@@ -40,42 +58,46 @@ function ShoppingCart() {
     <div className='container mx-auto p-4'>
       <div className='bg-white shadow-md rounded-lg p-4'>
         <h1 className='text-3xl text-center font-bold mb-4'>Basket</h1>
-        {user?.shoppingCart.length === 0 ? (
+        {cartItems && cartItems.length === 0 ? (
           <p>Your shopping cart is empty.</p>
         ) : (
           <div>
-            {user?.shoppingCart.map((item) => (
-              <div
-                key={item._id}
-                className='flex justify-between items-center mb-4'
-              >
-                <div>
-                  <h2 className='text-xl font-semibold'>{item.name}</h2>
-                  <p>Price: ${item.price.toFixed(2)}</p>
-                  {/* <p>Quantity: {item.}</p> */}
-                  <div className='flex items-center'>
-                    <button
-                      // onClick={() => increaseQuantity(item.id)}
-                      className='bg-green-500 text-white px-2 py-1 rounded mr-2'
-                    >
-                      +
-                    </button>
-                    <button
-                      // onClick={() => decreaseQuantity(item.id)}
-                      className='bg-yellow-500 text-white px-2 py-1 rounded'
-                    >
-                      -
-                    </button>
-                  </div>
-                </div>
-                <button
-                  // onClick={() => handleRemoveItem(item.id)}
-                  className='bg-red-500 text-white px-4 py-2 rounded'
+            {cartItems &&
+              cartItems.map((item) => (
+                <div
+                  key={item.product._id}
+                  className='flex justify-between items-center mb-4'
                 >
-                  Remove
-                </button>
-              </div>
-            ))}
+                  <div>
+                    {/* {console.log('item', item)} */}
+                    <h2 className='text-xl font-semibold'>
+                      {item.product.name}
+                    </h2>
+                    <p>Price: ${item.product.price.toFixed(2)}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <div className='flex items-center'>
+                      <button
+                        onClick={() => increaseQuantity(item.product._id)}
+                        className='bg-green-500 text-white px-2 py-1 rounded mr-2'
+                      >
+                        +
+                      </button>
+                      <button
+                        onClick={() => decreaseQuantity(item.product._id)}
+                        className='bg-yellow-500 text-white px-2 py-1 rounded'
+                      >
+                        -
+                      </button>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => deleteItem(item.product._id)}
+                    className='bg-red-500 text-white px-4 py-2 rounded'
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
             <div className='text-right font-bold text-xl'>
               {/* Total: ${totalPrice.toFixed(2)} */}
             </div>

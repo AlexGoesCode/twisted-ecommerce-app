1aabffd84263ffefae1697857f676a6aa44cf7de
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-12 18:16:32 +0200

    building Checkout.tsx

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,8 +1,20 @@
+import { useAuth } from '../context/AuthContext';
+
 const Checkout = () => {
+  const { user } = useAuth();
+  const userShoppingCart = user?.shoppingCart;
+  console.log('userShoppingCart :>> ', userShoppingCart);
   return (
     <div>
       <h1>New Page</h1>
-      <p>This is the new page content.</p>
+      {userShoppingCart &&
+        userShoppingCart.map((item) => {
+          return (
+            <p>
+              {item.product.name} : {item.product.price}
+            </p>
+          );
+        })}
     </div>
   );
 };

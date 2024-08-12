6e7971c5914e6c9526613fd4e73ba74d237d456c
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-12 18:21:26 +0200

    reordered; +/deleteItemsFromCart; +/checkout route

--- server/routes/itemsRouter.js
+++ server/routes/itemsRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import itemModel from '../models/itemModel.js';
-import { multerUpload } from '../middleware/multer.js';
+// import itemModel from '../models/itemModel.js';
+// import { multerUpload } from '../middleware/multer.js'; // useful for file upload route
 
 //* Import all the functions from the itemsController
 import {
   getCart,
   addItemsToCart,
   removeItemsFromCart,
+  deleteItemsFromCart,
   allItems,
   getItemById,
   itemsByCountry,
@@ -18,10 +19,13 @@ const itemsRouter = express.Router();
 //* Define the routes for the items API
 itemsRouter.get('/all', allItems); // ('/all', controller);
 itemsRouter.get('/france', itemsByCountry);
-itemsRouter.get('/:itemId', getItemById);
 itemsRouter.get('/cart', authMiddleware, getCart);
+itemsRouter.get('/:itemId', getItemById);
 itemsRouter.patch('/addItemsToCart', authMiddleware, addItemsToCart);
 itemsRouter.patch('/removeItemsFromCart', authMiddleware, removeItemsFromCart);
+itemsRouter.delete('/deleteItemsFromCart', authMiddleware, deleteItemsFromCart);
+itemsRouter.post('/checkout', authMiddleware /*checkout*/);
+// API list is ordered - /cart and then /:itemId
 
 // itemsRouter.put('/:itemid/rating', authMiddleware, rating);
 export default itemsRouter;

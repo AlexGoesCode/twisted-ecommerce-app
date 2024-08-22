6733ecd60f7e9ab570b9b24fad74b62209d77fda
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-22 16:56:54 +0200

    + error handling

--- server/utils/passwordServices.js
+++ server/utils/passwordServices.js
@@ -1,9 +1,14 @@
 import bcrypt from 'bcrypt';
 
 export const passwordEncryption = async (plainPassword) => {
-  console.log('plainPassword :>> ', plainPassword);
-  const saltRounds = 10; // 10 salts is an efficient standard, more salts takes exponentionally longer
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(plainPassword, salt);
-  return hashedPassword;
+  try {
+    console.log('plainPassword :>> ', plainPassword);
+    const saltRounds = 10; // 10 salts is an efficient standard, more salts takes exponentionally longer
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(plainPassword, salt);
+    return hashedPassword;
+  } catch (error) {
+    console.error('Error encrypting password :>> ', error);
+    throw new Error('Password encryåtion failed');
+  }
 };

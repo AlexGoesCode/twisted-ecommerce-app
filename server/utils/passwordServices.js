857bc446e2cbd85b59e482249af212c91fb718f9
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-08-23 11:14:56 +0200

    reverted export

--- server/utils/passwordServices.js
+++ server/utils/passwordServices.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
-export const passwordEncryption = async (plainPassword) => {
+const passwordEncryption = async (plainPassword) => {
   try {
     console.log('plainPassword :>> ', plainPassword);
     const saltRounds = 10; // 10 salts is an efficient standard, more salts takes exponentionally longer
@@ -12,3 +12,5 @@ export const passwordEncryption = async (plainPassword) => {
     throw new Error('Password encryption failed');
   }
 };
+
+export default passwordEncryption;

c077bb87c14812c42bbe1b41c81ddd71dbe6354d
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-06 16:36:00 +0200

    the container is styled well

--- client/src/pages/MyAccount.tsx
+++ client/src/pages/MyAccount.tsx
@@ -42,99 +42,105 @@ const MyAccount = () => {
   return (
     // <div className='container mx-auto p-4 md:p-20 w-full md:w-4/5 xl:w-3/5 mt-48 sm:mt-32 md:mt-32 lg:mt-32 xl:mt-32'>
     // <div className='relative bg-white shadow-lg rounded-2xl p-4 md:p-10 overflow-hidden min-h-96'></div>
-    <div className='relative bg-white p-4 sm:p-10 w-screen sm:w-3/5 xl:w-3/5 max-h-96 sm:h-96 mt-10 sm:mt-36 shadow-lg rounded-2xl overflow-hidden'>
-      <button
-        className='absolute top-2 right-2 w-16 md:w-20 bg-orange-300 p-2 md:p-3 rounded-full'
-        onClick={handleBack}
-      >
-        Back
-      </button>
-      <h1 className='text-xl md:text-2xl text-center font-bold mb-4'>
-        My Account
-      </h1>
-      <h2 className='text-lg md:text-xl font-semibold mb-2'>Orders History:</h2>
-      <div className='h-5/6 sm:h-96 overflow-y-auto p-2 md:p-4'>
-        {orders && orders.length > 0 ? (
-          orders.map((order) => (
-            <div
-              key={order._id}
-              className='border border-gray-300 rounded-lg p-2 md:p-4 mb-4 bg-gray-50'
-            >
-              <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-2'>
-                <div>
-                  <h3 className='text-sm md:text-md font-medium'>
-                    Order ID: {order._id}
-                  </h3>
-                  <p className='text-xs md:text-sm'>
-                    Date: {new Date(order.createdAt).toLocaleDateString()}
-                  </p>
-                  <p className='text-xs md:text-sm'>
-                    Total Price: $
-                    {order.items
-                      .reduce(
-                        (total, item) =>
-                          total + item.product.price * item.quantity,
-                        0
-                      )
-                      .toFixed(2)}
-                  </p>
-                </div>
-                <button
-                  className='text-blue-500 underline mt-2 md:mt-0'
-                  onClick={() => handleToggle(order._id)}
-                >
-                  {toggledOrderId === order._id
-                    ? 'Hide Details'
-                    : 'Show Details'}
-                </button>
-              </div>
-              {toggledOrderId === order._id && (
-                <div>
-                  {order.items && order.items.length > 0 ? (
-                    <>
-                      {order.items.map((item, index) => (
-                        <div
-                          key={index}
-                          className='border-t border-gray-200 pt-2 mt-2'
-                        >
-                          <p className='text-xs md:text-sm'>
-                            Product: {item.product.name}
-                          </p>
-                          <p className='text-xs md:text-sm'>
-                            Quantity: {item.quantity}
-                          </p>
-                          <p className='text-xs md:text-sm'>
-                            Price: $
-                            {item.product.price
-                              ? item.product.price.toFixed(2)
-                              : 'N/A'}
-                          </p>
-                        </div>
-                      ))}
-                    </>
-                  ) : (
+    <div className='flex justify-center items-center h-auto w-full'>
+      <div className='relative bg-white p-4 sm:p-10 w-full sm:w-3/5 xl:w-3/5 h-auto sm:h-3/5 mt-10 sm:mt-36 ml-2 sm:ml-0 shadow-lg rounded-2xl'>
+        <button
+          className='absolute top-2 right-2 w-16 md:w-20 bg-orange-300 p-2 md:p-3 rounded-full'
+          onClick={handleBack}
+        >
+          Back
+        </button>
+        <h1 className='text-xl md:text-2xl text-center font-bold mb-4'>
+          My Account
+        </h1>
+        <h2 className='text-lg md:text-xl font-semibold mb-2 ml-2'>
+          Orders History:
+        </h2>
+        <div className='h-96 sm:h-72 overflow-y-auto p-2 md:p-4'>
+          {orders && orders.length > 0 ? (
+            orders.map((order) => (
+              <div
+                key={order._id}
+                className='border border-gray-300 rounded-lg p-2 md:p-4 mb-4 bg-gray-50'
+              >
+                <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-2'>
+                  <div>
+                    <h3 className='text-sm md:text-md font-medium'>
+                      Order ID: {order._id}
+                    </h3>
                     <p className='text-xs md:text-sm'>
-                      No items in this order.
+                      Date: {new Date(order.createdAt).toLocaleDateString()}
                     </p>
-                  )}
-                  <div className='border-t border-gray-200 pt-2 mt-2'>
-                    <p className='text-xs md:text-sm font-medium'>
-                      Status: {order.status}
-                    </p>
-                    <p className='text-xs md:text-sm font-medium'>
-                      Shipping Address: {order.shippingAddress}
+                    <p className='text-xs md:text-sm'>
+                      Total Price: $
+                      {order.items
+                        .reduce(
+                          (total, item) =>
+                            total + item.product.price * item.quantity,
+                          0
+                        )
+                        .toFixed(2)}
                     </p>
                   </div>
+                  <button
+                    className='text-blue-500 underline mt-2 md:mt-0'
+                    onClick={() => handleToggle(order._id)}
+                  >
+                    {toggledOrderId === order._id
+                      ? 'Hide Details'
+                      : 'Show Details'}
+                  </button>
                 </div>
-              )}
-            </div>
-          ))
-        ) : (
-          <p className='text-center text-xs md:text-sm'>You have no orders.</p>
-        )}
+                {toggledOrderId === order._id && (
+                  <div>
+                    {order.items && order.items.length > 0 ? (
+                      <>
+                        {order.items.map((item, index) => (
+                          <div
+                            key={index}
+                            className='border-t border-gray-200 pt-2 mt-2'
+                          >
+                            <p className='text-xs md:text-sm'>
+                              Product: {item.product.name}
+                            </p>
+                            <p className='text-xs md:text-sm'>
+                              Quantity: {item.quantity}
+                            </p>
+                            <p className='text-xs md:text-sm'>
+                              Price: $
+                              {item.product.price
+                                ? item.product.price.toFixed(2)
+                                : 'N/A'}
+                            </p>
+                          </div>
+                        ))}
+                      </>
+                    ) : (
+                      <p className='text-xs md:text-sm'>
+                        No items in this order.
+                      </p>
+                    )}
+                    <div className='border-t border-gray-200 pt-2 mt-2'>
+                      <p className='text-xs md:text-sm font-medium'>
+                        Status: {order.status}
+                      </p>
+                      <p className='text-xs md:text-sm font-medium'>
+                        Shipping Address: {order.shippingAddress}
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <p className='text-center text-xs md:text-sm'>
+              You have no orders.
+            </p>
+          )}
+        </div>
       </div>
+      //{' '}
     </div>
-    // </div>
   );
 };
 

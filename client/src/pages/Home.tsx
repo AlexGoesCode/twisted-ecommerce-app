b2f20f8b2e06ab3fe44d62b2c0b849f3e5fec7e3
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-06 14:05:19 +0200

    styles

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,8 +6,8 @@ const Home = () => {
   //   const { user } = useContext(AuthContext);
 
   return (
-    <div className='relative px-5 inset-0 z-10 flex items-center justify-center'>
-      <div className='bg-white opacity-95 max-w-2xl h-auto p-10 mt-36 rounded-2xl shadow-lg text-center'>
+    <div className='px-5 inset-0 z-10 flex items-center justify-center'>
+      <div className='bg-white opacity-95 max-w-2xl h-auto p-10 mt-4 md:mt-32 rounded-2xl shadow-lg text-center'>
         <h1 className='text-2xl font-bold mb-4'>Welcome to BobbleHeads!</h1>
         <p className='mt-4 text-xl'>
           Finally you can take a piece of your favorite (or least favorite)

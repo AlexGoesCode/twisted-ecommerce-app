a77825fda86acd65a949be80f5ce99281e4f06b8
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-10 17:18:35 +0200

    baseUrl for two modes

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,6 @@
-export const baseUrl = import.meta.env.VITE_BASE_URL;
+// export const baseUrl = import.meta.env.VITE_BASE_URL;
+
+export const baseUrl =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5022/api'
+    : 'https://bobbleheads.vercel.app/api';

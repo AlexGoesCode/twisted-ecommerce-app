8afb2b65ead425560a998373ae1a8db2dbd3e355
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-11 13:53:45 +0200

    URL config upd

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,5 @@
-// export const baseUrl = import.meta.env.VITE_BASE_URL;
+const isDevelopment = import.meta.env.MODE === 'development';
 
-export const baseUrl =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:5022/api'
-    : 'https://bobbleheads.vercel.app/api';
+export const baseUrl = isDevelopment
+  ? import.meta.env.VITE_LOCAL_BASE_URL
+  : import.meta.env.VITE_PROD_BASE_URL;

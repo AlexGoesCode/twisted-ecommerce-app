a5ddcf95f91470206af299fedde0c51eb8f16424
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-12 12:49:36 +0200

    config.ts update contains 2 url in different MODE

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,7 @@
 // export const baseUrl = "http://localhost:5022/api";
+// export const baseUrl = 'https://bobbleheads-server.vercel.app/api';
 
-export const baseUrl = 'https://bobbleheads-server.vercel.app/api';
+export const baseUrl =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5022/api'
+    : 'https://bobbleheads-server.vercel.app/api';

2edb447c3c62afeceda0285d7c4fcf92037da54d
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-15 20:26:58 +0200

    avatar menu 2nd upd

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -167,7 +167,7 @@ const Navbar = () => {
                     leaveFrom='transform opacity-100 scale-100'
                     leaveTo='transform opacity-0 scale-95'
                   >
-                    <MenuItems className='absolute right-0 z-10 mt-2 w-42 sm:w-48 origin-top-right rounded-md bg-white py-3 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                    <MenuItems className='absolute right-0 z-10 mt-1 w-56 sm:w-48 origin-top-right rounded-md bg-white py-3 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                       {isAuthenticated ? (
                         <>
                           <MenuItem>

661ddba85cef91fdd131b48949b2b53c54014e54
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-15 21:15:22 +0200

    z index adjusted

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -48,13 +48,13 @@ function SearchBar({
 
   return (
     <form
-      className={`max-w-lg mx-auto my-8 sm:relative ${className}`}
+      className={`max-w-lg mx-auto my-8 relative ${className}`}
       onSubmit={handleSubmit}
     >
       <div className='flex'>
         <select
           id='dropdown-button'
-          className='z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 relative'
+          className='relative z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600'
           value={searchBy}
           onChange={handleOptionChange}
         >
@@ -73,7 +73,7 @@ function SearchBar({
         />
         <button
           type='submit'
-          className='p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          className='relative p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
         >
           <svg
             className='w-4 h-4'
